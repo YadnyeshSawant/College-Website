a04701d8fc231ecb4fecea52ed4746957822036f
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2024-01-06 21:27:39 +0530

    j-Queries for portal transission

--- PAGES/app.js
+++ PAGES/app.js
@@ -9,3 +9,18 @@ prev.addEventListener('click', function(){
     let items =document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1 ])
 })
+
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
+	})
+});
+
+
